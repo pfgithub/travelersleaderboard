3660f79aa8088f5c5dc7da239109fbe427ea089d
pfg <omegariisthebest@gmail.com>
2020-02-13 20:32:36 -0800

    fix waiting too long and add countdown

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const fetch = require("node-fetch");
-const {parse} = require("node-html-parser");
+const { parse } = require("node-html-parser");
 const fs = require("fs");
 const path = require("path");
 
 let run = async () => {
-    
-    let fr = await fetch("https://thetravelers.online/leaderboard");
-    const dom = parse(await fr.text());
-    //console.log(dom.structure);
-    let leaderData = (dom.querySelector("#leaderJSON").attributes.value);
-    let minutesLeft = (dom.querySelector("#minutesLeft").text);
+  console.log("Fetching!...");
+  let fr = await fetch("https://thetravelers.online/leaderboard");
+  const dom = parse(await fr.text());
+  //console.log(dom.structure);
+  let leaderData = dom.querySelector("#leaderJSON").attributes.value;
+  let minutesLeft = dom.querySelector("#minutesLeft").text;
 
-    fs.appendFileSync(path.join(__dirname, "history.txt"), leaderData + "\n\n" + "====" + "\n\n");
-    
-    console.log("minutes: ",minutesLeft);
+  fs.appendFileSync(
+    path.join(__dirname, "history.txt"),
+    leaderData + "\n\n" + "====" + "\n\n"
+  );
 
-    setTimeout(() => run(), +(minutesLeft + 3) * 60 * 1000);
+  console.log(leaderData);
+  let delayTime = (+minutesLeft + 3) * 60 * 1000;
+  console.log("Done!", minutesLeft, delayTime);
+  queue(delayTime);
 };
+
+function queue(timems) {
+  let start = new Date().getTime();
+  let intv = setInterval(
+    () =>
+      process.stdout.write(
+        "\r" + (timems - (new Date().getTime() - start)) + "ms left\u001b[0K"
+      ),
+    1000
+  );
+  setTimeout(() => {
+    clearInterval(intv);
+    console.log("");
+    run();
+  }, timems);
+}
 
 run();
