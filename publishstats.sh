bab161d081de167db194cd73fd606ec5bee2b970
pfg <pfg@pfg.pw>
2020-09-04 16:27:11 -0700

    dsds

--- publishstats.sh
+++ publishstats.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd gh-pages/travelersleaderboard
 cp ../../history.txt history.txt
-git add history.txt
+git reset --soft HEAD~1
+git add --all
 git commit -m "update history"
-git push
+git push --force
